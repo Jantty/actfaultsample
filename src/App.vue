99ab9aacd1f3816f60be4cfe5ad9bcd18daeefe2
Jantty <jantty_y@qq.com>
2021-11-26 23:57:01 +0800

    增加elment ui container

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <el-container class="app-out-pannel">
+            <el-header class="sys-header">活断层项目系统</el-header>
+            <el-container class="app-content-pannel">
+                <el-aside width="200px" class="sys-menu">左侧菜单</el-aside>
+                <el-main>地图区域</el-main>
+            </el-container>
+        </el-container>
     </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
     name: 'App',
-    components: {
-        HelloWorld,
-    },
+    components: {},
 };
 </script>
 
@@ -32,5 +33,18 @@ body,
     width: 100%;
     height: 100%;
     margin: 0;
+}
+.app-out-pannel,
+.app-content-pannel {
+    height: 100%;
+}
+.sys-header {
+    background-color: #409eff;
+    line-height: 60px;
+    color: aliceblue;
+    font-size: 20px;
+}
+.sys-menu {
+    background-color: #c0c4cc;
 }
 </style>
