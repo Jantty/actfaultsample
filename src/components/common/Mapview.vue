a0b1e8798bcd1d0823b3660b09f6a8218a99e64d
Jantty <jantty_y@qq.com>
2021-11-27 21:16:16 +0800

    增加地图控件

--- src/components/common/Mapview.vue
+++ src/components/common/Mapview.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="mapview-pannel">
         <div id="mapview"></div>
+        <div id="basemapToggle"></div>
+        <div id="scaleBar"></div>
+        <div id="zoom"></div>
     </div>
 </template>
 
@@ -22,12 +25,15 @@ export default {
             // document.getElementById('basemapToggle').innerHTML = '';
             // document.getElementById('scaleBar').innerHTML = '';
             // document.getElementById('zoom').innerHTML = '';
-            const [Map, MapView, Basemap, TileLayer] = await loadModules(
+            const [Map, MapView, Basemap, TileLayer, BasemapToggle, ScaleBar, Zoom] = await loadModules(
                 [
                     'esri/Map',
                     'esri/views/MapView',
                     'esri/Basemap',
                     'esri/layers/TileLayer',
+                    'esri/widgets/BasemapToggle',
+                    'esri/widgets/ScaleBar',
+                    'esri/widgets/Zoom',
                     // 'esri/widgets/BasemapToggle',
                     // 'esri/widgets/ScaleBar',
                     // 'esri/widgets/Zoom',
@@ -57,6 +63,29 @@ export default {
                 center: [104.072745, 30.663774],
             });
 
+            //实例化底图切换
+            const basemapToggle = new BasemapToggle({
+                view: mapView,
+                nextBasemap: 'hybrid',
+                container: 'basemapToggle',
+            });
+            mapView.ui.add(basemapToggle);
+
+            //实例化比例尺
+            const scaleBar = new ScaleBar({
+                view: mapView,
+                container: 'scaleBar',
+                unit: 'metric',
+            });
+            mapView.ui.add(scaleBar);
+
+            //实例化地图缩放控件
+            const zoom = new Zoom({
+                view: mapView,
+                container: 'zoom',
+            });
+            mapView.ui.add(zoom);
+
             mapView.ui.components = [];
             // this.$store.ommit('_setDefaultView', mapView);
             console.log(mapView);
@@ -71,5 +100,20 @@ export default {
     position: relative;
     width: 100%;
     height: 100%;
+}
+#basemapToggle {
+    position: absolute;
+    right: 15px;
+    bottom: 15px;
+}
+#scaleBar {
+    position: absolute;
+    left: 15px;
+    bottom: 15px;
+}
+#zoom {
+    position: absolute;
+    right: 15px;
+    bottom: 100px;
 }
 </style>
