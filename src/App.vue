8909dc383e40ecffb4dc1e1d6a47ed5550cdc9dd
Jantty <jantty_y@qq.com>
2021-11-27 10:40:19 +0800

    优化首页布局

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,21 @@
         <el-container class="app-out-pannel">
             <el-header class="sys-header">活断层项目系统</el-header>
             <el-container class="app-content-pannel">
-                <el-aside width="200px" class="sys-menu">左侧菜单</el-aside>
+                <el-aside width="200px" class="sys-menu">
+                    <el-menu
+                        default-active="1"
+                        class="el-menu-vertical-demo"
+                        @select="handleMenuSelect"
+                        @open="handleOpen"
+                        @close="handleClose"
+                        :collapse="true"
+                    >
+                        <el-menu-item index="1" class="sys-menu-item">
+                            <i class="el-icon-map-location"></i>
+                            <span slot="title">活断层专题图</span>
+                        </el-menu-item>
+                    </el-menu>
+                </el-aside>
                 <el-main class="sys-content">
                     <Mapview />
                 </el-main>
@@ -18,6 +32,11 @@ export default {
     name: 'App',
     components: {
         Mapview,
+    },
+    data() {
+        return {
+            // isCollapse: true,
+        };
     },
 };
 </script>
@@ -44,13 +63,23 @@ body,
     height: 100%;
 }
 .sys-header {
-    background-color: #409eff;
+    background-color: #32373e;
     line-height: 60px;
-    color: aliceblue;
+    color: #fff;
     font-size: 20px;
+    font-weight: 600;
+    display: flex;
+    justify-content: space-between;
 }
 .sys-menu {
-    background-color: #c0c4cc;
+    background-color: #545c64;
+    width: 64px !important;
+    overflow: hidden !important;
+}
+.sys-menu-item {
+    background-color: #545c64;
+    /* text-color: '#fff';
+    active-text-color: '#ffd04b'; */
 }
 .sys-content {
     padding: 5px !important;
